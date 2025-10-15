8ae3ef2637836e79e5981b667411b2e5ac6dcddc
Biriii <bangeabar@gmail.com>
2025-10-15 14:57:16 +0700

    up

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="@yield('description', 'Hệ thống quản lý xe máy chuyên nghiệp')">
     <meta name="author" content="QLXM">
+    <link rel="shortcut icon" href="{{ asset('img/icons/icon-48x48.png') }}" />
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap"
         rel="stylesheet">
 
